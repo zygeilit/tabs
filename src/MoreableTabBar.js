4ebc69e1ffc7280e2e2fe4e6683b64d830e8dd46
zhangyue <zhangyue@beisen.com>
2019-04-10 11:43:02 +0800

    fist 2

--- src/MoreableTabBar.js
+++ src/MoreableTabBar.js
@@ -3,22 +3,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import InkTabBarNode from './InkTabBarNode';
 import MoreableTabBarTabsNode from './MoreableTabBarTabsNode';
-import MoreableTabBarNode from './MoreableTabBarNode'
 import TabBarRootNode from './TabBarRootNode';
 import SaveRef from './SaveRef';
 
 export default class ScrollableInkTabBar extends React.Component {
   render() {
     const { children: renderTabBarNode, ...restProps } = this.props;
-
     return (
       <SaveRef>
         {(saveRef, getRef) => (
           <TabBarRootNode saveRef={saveRef} {...restProps}>
-            <MoreableTabBarNode saveRef={saveRef} getRef={getRef} {...restProps}>
-              <MoreableTabBarTabsNode saveRef={saveRef} renderTabBarNode={renderTabBarNode} {...restProps} />
-              <InkTabBarNode saveRef={saveRef} getRef={getRef} {...restProps} />
-            </MoreableTabBarNode>
+            <MoreableTabBarTabsNode saveRef={saveRef} getRef={getRef} renderTabBarNode={renderTabBarNode} {...restProps} />
+            <InkTabBarNode saveRef={saveRef} getRef={getRef} {...restProps} />
           </TabBarRootNode>
         )}
       </SaveRef>
--- src/MoreableTabBarTabsNode.js
+++ src/MoreableTabBarTabsNode.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import warning from 'warning';
 import PropTypes from 'prop-types';
+import ResizeObserver from 'resize-observer-polyfill';
+import debounce from 'lodash/debounce';
 import { isVertical } from './utils';
 
 export default class TabBarTabsNode extends React.Component {
-  render() {
+
+  containerPlaceholderRef = React.createRef()
+
+  state = {
+    'lastItemIndex': 0,
+    'hasMoreItem': false,
+  }
+
+  componentDidMount() {
+    this.debouncedResize = debounce(() => {
+      this.refreshTabNav()
+    }, 100);
+    this.resizeObserver = new ResizeObserver(this.debouncedResize);
+    this.resizeObserver.observe(this.props.getRef('navTabsContainer'));
+  }
+
+  refreshTabNav() {
+    const { prefixCls, 'panels': children, getRef } = this.props;
+    const containerWidth = getRef('navTabsContainer').offsetWidth;
+    const items = this.containerPlaceholderRef.current.querySelectorAll(`.${prefixCls}-tab`);
+    let hasMoreItem = false;
+    let lastItemIndex = children.length;
+
+    for (let i=0; i<items.length; i++) {
+      const restWidth = containerWidth - items[i].offsetLeft;
+      if (restWidth < 360) {
+        hasMoreItem = true;
+        lastItemIndex = i-1;
+        break;
+      }
+    }
+    this.setState({ hasMoreItem, lastItemIndex });
+  }
+
+  renderItemMoreBtn() {
+    const { 'panels': children } = this.props
+    const moreBtn = React.cloneElement(children[0], {
+      'key': 99999,
+      'disabled': false,
+      'tab': '...'
+    })
+    return this.renderChildItem(moreBtn, children.length, true)
+  }
+
+  renderChildItem(child, index, isMoreBtn) {
     const {
       'panels': children,
       activeKey,
@@ -14,59 +60,79 @@ export default class TabBarTabsNode extends React.Component {
       tabBarPosition,
       renderTabBarNode,
     } = this.props;
-    const rst = [];
 
-    React.Children.forEach(children, (child, index) => {
-      if (!child) {
-        return;
-      }
-      const key = child.key;
-      let cls = activeKey === key ? `${prefixCls}-tab-active` : '';
-      cls += ` ${prefixCls}-tab`;
-      let events = {};
+    if (!child) {
+      return;
+    }
+    const key = child.key;
+    let cls = activeKey === key ? `${prefixCls}-tab-active` : '';
+    cls += ` ${prefixCls}-tab`;
+    let events = {};
+
+    if (!isMoreBtn) {
       if (child.props.disabled) {
         cls += ` ${prefixCls}-tab-disabled`;
       } else {
         events = {
-          onClick: this.props.onTabClick.bind(this, key),
+          'onClick': this.props.onTabClick.bind(this, key),
         };
       }
-      const ref = {};
-      if (activeKey === key) {
-        ref.ref = saveRef('activeTab');
-      }
+    }
 
-      const gutter = tabBarGutter && index === children.length - 1 ? 0 : tabBarGutter;
-      const style = {
-        [isVertical(tabBarPosition) ? 'marginBottom' : 'marginRight']: gutter,
-      };
-      warning('tab' in child.props, 'There must be `tab` property on children of Tabs.');
+    const ref = {};
+    if (activeKey === key) {
+      ref.ref = saveRef('activeTab');
+    }
 
-      let node = (
-        <div
-          role="tab"
-          aria-disabled={child.props.disabled ? 'true' : 'false'}
-          aria-selected={activeKey === key ? 'true' : 'false'}
-          {...events}
-          className={cls}
-          key={key}
-          style={ style }
-          {...ref}
-        >
-          {child.props.tab}
-        </div>
-      );
+    const gutter = tabBarGutter && index === children.length - 1 ? 0 : tabBarGutter;
+    const style = {
+      [isVertical(tabBarPosition) ? 'marginBottom' : 'marginRight']: gutter,
+    };
+    warning('tab' in child.props, 'There must be `tab` property on children of Tabs.');
 
-      if (renderTabBarNode) {
-        node = renderTabBarNode(node);
-      }
+    let node = (
+      <div
+        role="tab"
+        aria-disabled={child.props.disabled ? 'true' : 'false'}
+        aria-selected={activeKey === key ? 'true' : 'false'}
+        {...events}
+        className={cls}
+        key={key}
+        style={ style }
+        {...ref}
+      >
+        {child.props.tab}
+      </div>
+    );
 
-      rst.push(node);
-    });
+    if (renderTabBarNode) {
+      node = renderTabBarNode(node);
+    }
+
+    return node;
+  }
+
+  render() {
+    const {
+      hasMoreItem,
+      lastItemIndex,
+    } = this.state;
+    const {
+      'panels': children,
+      prefixCls,
+      saveRef,
+    } = this.props;
+    const rst = [];
+
+    React.Children.forEach(children, (child, index) => rst.push(this.renderChildItem(child, index)));
 
     return (
-      <div ref={saveRef('navTabsContainer')}>
-        {rst}
+      <div className={`${prefixCls}-moreable-nav`} ref={saveRef('navTabsContainer')}>
+        <div>
+          { rst.slice(0, lastItemIndex+1) }
+          { hasMoreItem && this.renderItemMoreBtn() }
+        </div>
+        <div className={`${prefixCls}-moreable-nav-placeholder`} ref={this.containerPlaceholderRef}>{rst}</div>
       </div>
     );
   }
